797cf0a9d79e8644f88dbee62874bf9442f22874
o-star <uh04049@naver.com>
2020-12-21 22:55:35 +0900

    React add

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-    res.send('Hello world');
+app.get('/api', (req, res) => {
+    res.send({title:"ostar !"});
 })
 
 app.listen(port, () => {
