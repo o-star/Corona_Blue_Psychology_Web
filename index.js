21ad1a2fc35dad913c9a3f17cf27a6e24b7526f7
o-star <uh04049@naver.com>
2020-12-22 18:11:10 +0900

    react build process

--- index.js
+++ index.js
@@ -9,3 +9,11 @@ app.get('/api', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is listening at localhost:${process.env.PORT}`);
 })
+
+// 리액트 정적 파일 제공
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// 라우트 설정
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
