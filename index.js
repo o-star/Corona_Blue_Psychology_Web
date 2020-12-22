b5f637fa20356b867a823c1dd68b19626b663d3f
o-star <uh04049@naver.com>
2020-12-22 18:17:39 +0900

    react build2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
+const path = require('path');
 
 app.get('/api', (req, res) => {
     res.send({title:"ostar !"});
