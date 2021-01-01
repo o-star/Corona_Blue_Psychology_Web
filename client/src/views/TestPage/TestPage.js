52205e09ba39f93278c930263af643d116862a2b
o-star <uh04049@naver.com>
2021-01-01 19:07:08 +0900

    psychology test table add

--- client/src/views/TestPage/TestPage.js
+++ client/src/views/TestPage/TestPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 // nodejs library that concatenates classes
 import classNames from "classnames";
 // @material-ui/core components
@@ -15,21 +15,13 @@ import Parallax from "components/Parallax/Parallax.js";
 import circleimg from "assets/img/circle_image.jpg";
 import styles from "assets/jss/material-kit-react/views/profilePage.js";
 
-import axios from "axios";
+import { questiontable, replytable } from "./TestItemTable.js"
 
 const useStyles = makeStyles(styles);
 
 
 export default function TestPage(props) {
-
-  axios.post("/test-page")
-  .then(response => {
-     console.log(response);
-  })
-  .catch(error => {
-     console.log(error);
-  });
-
+  const [number, setNumber] = useState(1);
   const classes = useStyles();
   const { ...rest } = props;
   const imageClasses = classNames(
@@ -39,10 +31,11 @@ export default function TestPage(props) {
   );
   const btnStyle = {
     justify: "center",
-    color: "yellow",
-    background: "teal",
-    padding: "1rem 15em",
-    border: "1px solid teal",
+    color: "white",
+    background: "#22abe6",
+    padding: "1rem 0rem",
+    width: "400px",
+    border: "1px solid #22abe6",
     borderRadius: "3rem",
     fontSize: "1.1rem",
     lineHeight: 1.5,
@@ -50,6 +43,9 @@ export default function TestPage(props) {
   const gridStyle = {
     margin: "3px auto",
     textAlign: "center"
+  }
+  const clickEvent = () => {
+    setNumber(number + 1);
   }
   // const navImageClasses = classNames(classes.imgRounded, classes.imgGallery);
   return (
@@ -72,7 +68,11 @@ export default function TestPage(props) {
             <GridContainer justify="center">
               <GridItem xs={12} sm={12} md={6}>
                 <div className={classes.profile}>
-                  <br/><br/><br/><br/><br/>
+                  <br />
+                  <div className={classes.textCenter} justify="center">
+                    <h2>코로나 블루 우울 심리 테스트</h2>
+                  </div>
+                  <br /><br /><br /><br /><br />
                   <div>
                     <img src={circleimg} alt="..." className={imageClasses} />
                   </div>
@@ -80,23 +80,26 @@ export default function TestPage(props) {
               </GridItem>
             </GridContainer>
             <div className={classes.description}>
-              <p>
-                An artist of considerable range, Chet Faker — the name taken by
-                Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs
-                and records all of his own music, giving it a warm, intimate
-                feel with a solid groove structure.{" "}
-              </p>
+              <h3>
+                {questiontable[number]}
+              </h3>
             </div>
             <GridContainer justify="center">
               <GridItem style={gridStyle}>
-                <Button style={btnStyle}>yes!</Button>
+                <Button onClick={clickEvent} style={btnStyle}>{replytable[number][0]}</Button>
               </GridItem>
               <GridItem style={gridStyle}>
-                <Button style={btnStyle}>no!</Button>
+                <Button onClick={clickEvent} style={btnStyle}>{replytable[number][1]}</Button>
+              </GridItem>
+              <GridItem style={gridStyle}>
+                <Button onClick={clickEvent} style={btnStyle}>{replytable[number][2]}</Button>
+              </GridItem>
+              <GridItem style={gridStyle}>
+                <Button onClick={clickEvent} style={btnStyle}>{replytable[number][3]}</Button>
               </GridItem>
             </GridContainer>
           </div>
-          <br/><br/>
+          <br /><br />
         </div>
       </div>
       <Footer />
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 
 app.post('/test-page', (req, res) => {
-  console.log("!");
+  console.log(req);
   res.send(req.body.number);
 })
 
